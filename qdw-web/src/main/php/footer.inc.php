d2865eb9cefd2e8dca5f8c6c9733dfed5ca96892
Kane Small <kane.small@stfc.ac.uk>
2013-04-15 15:04:09 +0100

    Changed the copyright notice year to 2013 and removed the 'design by Node Thirty Three Designs' notice

--- qdw-web/src/main/php/footer.inc.php
+++ qdw-web/src/main/php/footer.inc.php
@@ -4,6 +4,9 @@
     </div>
 </div>
 
-<div id="footer" class="fixed">Copyright &copy; 2012 quattor.org. Design by <a href="http://www.freecsstemplates.org">FCT</a> + <a href="http://www.nodethirtythree.com/">NodeThirtyThree Design</a>.</div>
+<div id="footer" class="fixed">
+	Copyright &copy; 2013 quattor.org.
+</div>
+
 </body>
 </html>
