628b7e30bd04a27c40e9cf27f3a55d28c26c4cd5
Kane Small <kane.small@stfc.ac.uk>
2013-04-16 15:06:09 +0100

    Update 'chdir' with the new qdw.py file location

--- qdw-web/src/main/php/DistributionIndex-json.php
+++ qdw-web/src/main/php/DistributionIndex-json.php
@@ -2,7 +2,7 @@
     header('Content-type: application/json');
     if (array_key_exists("attribute", $_REQUEST)) {
         $attribute = preg_replace("/[^A-Za-z0-9\/-_]/","",$_REQUEST["attribute"]);
-        chdir('qdwprogram');
+        chdir('/var/www/data-warehouse/qdw/src/main/python/');
         system('./qdw.py -n -d '.$attribute);
     }
     else {
