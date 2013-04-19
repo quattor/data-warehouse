ffe7dc69a5ebd023e9d3ec57a16f4d7efd126287
Kane Small <kane.small@stfc.ac.uk>
2013-04-19 15:21:07 +0100

    Split the getData() functions into two separate ones to then allow the button to be disabled and enabled based on whether there are any characters in both the attribute and the value input boxes

--- qdw-web/src/main/php/ServerAttribute.php
+++ qdw-web/src/main/php/ServerAttribute.php
@@ -14,10 +14,10 @@
 				<div class="input-append">
 					<input class="input-xxlarge" data-toggle="tooltip" data-placement="top" data-animation="true" title="Path to a value, e.g. /hardware/model" type="text" placeholder="Attribute" name="attribute" id="attribute" onkeydown="if (event.keyCode == 13) getData()" value="<?php echo $_GET["attribute"] ?>"/>
 					<input class="input-xlarge" data-toggle="tooltip" data-placement="top" data-animation="true" title="A value of an attribute, e.g. wn-2007-streamline" type="text" placeholder="Value" name="value" id="value" onkeydown="if (event.keyCode == 13) getData()" value="<?php echo $_GET["value"] ?>"/>
-					<button class="btn btn-primary" type="button" data-loading-text="Loading..." id="Search" onclick="getData()">Quattorise</button>
+					<button class="btn btn-primary" type="button" data-loading-text="Loading..." id="search" onclick="getData()">Quattorise</button>
 				</div>
 			</form>
-			
+
 			<div class="results" id="results">
 				<div class="loading" id="loading"></div>
 				<table class="table table-striped table-bordered" id="resultstable"></table>
@@ -33,17 +33,73 @@
 						$( "#attribute" ).autocomplete("search", "");
 					});
 				});
-				
+
 				//Pops open the info box for a specific node
 				var nodewin = null;
 				function node(n) {
-				  nodewin = window.open(INFO_URL+n, "node", "width=640,height=480,left=128,top=128,resizable=yes,scrollbars=yes,directories=no,titlebar=no,toolbar=no,status=no"); 
+				  nodewin = window.open(INFO_URL+n, "node", "width=640,height=480,left=128,top=128,resizable=yes,scrollbars=yes,directories=no,titlebar=no,toolbar=no,status=no");
 				  nodewin.window.focus();
+				}
+
+				function validateData() {
+					var x = document.forms["Search"]["attribute"].value;
+					var y = document.forms["Search"]["value"].value;
+					if (x == null || x == "" || y == null || y == "") {
+						$('#search').prop("disabled", true);
+						return false;
+					}
+					else {
+						$('#search').prop("disabled", false);
+						return true;
+					}
+				}
+
+				function getData() {
+					if (validateData()) {
+						var x = $("#attribute").val();
+						if (x[0] !== "/") {
+							x = "/" + x;
+						}
+						$("#resultstable_wrapper").hide();
+						$("#loading").show();
+						$('#search').prop("disabled", true);
+						$("#attribute").prop("disabled", true);
+						$("#value").prop("disabled", true);
+						$.get(
+							'ServerAttribute-json.php',
+							{ attribute: document.forms["Search"]["attribute"].value, value: document.forms["Search"]["value"].value },
+							function(response, status, xhr) {
+								var data = eval(response);
+								var oTable = $("#resultstable").dataTable();
+								oTable.fnClearTable();
+								oTable.fnAddData(data);
+								$('#search').prop("disabled", false);
+								$("#attribute").prop("disabled", false);
+								$("#value").prop("disabled", false);
+								$("#loading").hide();
+								$("#resultstable_wrapper").show();
+							}
+						);
+					}
 				}
 
 				$(document).ready(function() {
 					$("#attribute").tooltip();
 					$("#value").tooltip();
+					$("#attribute").keyup(function() {
+						validateData();
+					});
+					$("#attribute").change(function() {
+						validateData();
+					});
+					$("#value").keyup(function() {
+						validateData();
+					});
+					$("#value").change(function() {
+						validateData();
+					});
+					validateData();
+					$("#search").click(getData);
 					$('#resultstable').dataTable( {
 						"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
 						"fnRowCallback": function( nRow, aData, iDisplayIndex ) {
@@ -68,39 +124,6 @@
 					} );
 					getData();
 				});
-				
-				function getData() {
-					var x = document.forms["Search"]["attribute"].value;
-					var y = document.forms["Search"]["value"].value;
-					if (x == null || x == "") {
-						return false
-					}
-					else if (y == null || y == "") {
-						return false
-					}
-					else {
-						$("#resultstable_wrapper").hide();
-						$("#loading").show();
-						document.getElementById("Search").disabled=true;
-						document.getElementById("attribute").disabled=true;
-						document.getElementById("value").disabled=true;        
-						$.get(
-							'ServerAttribute-json.php',
-							{ attribute: document.forms["Search"]["attribute"].value, value: document.forms["Search"]["value"].value },
-							function(response, status, xhr) {
-								var data = eval(response);
-								var oTable = $("#resultstable").dataTable();
-								oTable.fnClearTable();
-								oTable.fnAddData(data);
-								document.getElementById("Search").disabled=false;
-								document.getElementById("attribute").disabled=false;
-								document.getElementById("value").disabled=false;
-								$("#loading").hide();
-								$("#resultstable_wrapper").show();
-							}
-						);
-					}
-				}
 			</script>
 		</div>
 	</div>
