7233396ba259f70fdba95525f23c712efb71575e
Kane Small <kane.small@stfc.ac.uk>
2013-04-19 10:48:13 +0100

    Compress all .css and .js files to .min.css and .min.js files

--- qdw-web/src/main/php/header.inc.php
+++ qdw-web/src/main/php/header.inc.php
@@ -5,21 +5,18 @@
 	<title>Quattor | Data Warehouse</title>
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
-	<link rel="stylesheet" type="text/css" href="css/styling.css" />
+	<link rel="stylesheet" type="text/css" href="css/styling.min.css" />
 	<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css" />
 	<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css" />
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" />
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/pygments.css" />
 	<script type="text/javascript" charset="utf-8" src="js/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript" charset="utf-8" src="js/jquery-ui.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/jquery.ui.core.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/keylist.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/keylist.min.js"></script>
 	<script type="text/javascript" charset="utf-8" src="js/jquery.ui.autocomplete.min.js"></script>
 	<script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap-button.js"></script>
-	<script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap-alert.js"></script>
-	<script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap-tooltip.js"></script>
+	<script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap-alert.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap-tooltip.min.js"></script>
 	<style type="text/css">
 		@import url(http://fonts.googleapis.com/css?family=Lato:400);
 		body {
