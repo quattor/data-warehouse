c8acb5bc61b6140eeb7620ea069a661ef367d0be
James Adams <james.adams@stfc.ac.uk>
2013-04-29 14:36:30 +0100

    Don't change directory before executing datawarehouse scripts
    We should expect the script to be on PATH.
    Also unify coding style and whitespace while we're at it.

--- qdw-web/src/main/php/api-json-distribution.php
+++ qdw-web/src/main/php/api-json-distribution.php
@@ -1,9 +1,9 @@
 <?php
     header('Content-type: application/json');
+
     if (array_key_exists("attribute", $_REQUEST)) {
         $attribute = preg_replace("/[^A-Za-z0-9\/-_]/","",$_REQUEST["attribute"]);
-        chdir('/var/www/data-warehouse/qdw/src/main/python/');
-        system('./qdw.py -n -d '.$attribute);
+        system('qdw.py -n -d '.$attribute);
     }
     else {
         echo "[['Profiles have yet to be analysed','1']]";
